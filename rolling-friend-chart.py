abca46f050f220df836d8053262f1a527e4f2091
Mauricio Villa <vbamauricio@gmail.com>
2022-02-18 11:02:57 -0300

    Improved styling and interpolation

--- rolling-friend-chart.py
+++ rolling-friend-chart.py
@@ -15,23 +15,27 @@ dictList = []
 #LastFM API
 lastfmApi = 'http://ws.audioscrobbler.com/2.0/'
 lastfmHeaders = {
-    'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36'
+    'User-Agent':
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36'
 }
 
 #Getting daily, weekly, monthly, yearly chart of each user in the list
 n = 0
 for user in usersList:
-    n = n + 1
-    print('Collecting the songs by', user, f'{n}-{len(usersList)}')
+    n += 1
+    print(f'Collecting the songs by "{user}" {n}-{len(usersList)}')
     lastfmParams = {
-        'method': 'user.gettoptracks', #user.gettoptracks, user.gettopalbums, user.gettopartists
+        'method':
+        'user.gettoptracks',  #user.gettoptracks, user.gettopalbums, user.gettopartists
         'user': user,
-        'period': '7day', #overall, 7day, 1month, 3month, 6month, 12month 
-        'limit': 1000, #1 to 1000
+        'period': '7day',  #overall, 7day, 1month, 3month, 6month, 12month
+        'limit': 1000,  #1 to 1000
         'api_key': apiKey,
         'format': 'json'
     }
-    lastfmResp = requests.get(lastfmApi, params = lastfmParams, headers = lastfmHeaders).text
+    lastfmResp = requests.get(lastfmApi,
+                              params=lastfmParams,
+                              headers=lastfmHeaders).text
     lastfmJson = json.loads(lastfmResp)
     mainkey = list(lastfmJson.keys())[0]
     childkey = list(lastfmJson[mainkey].keys())
@@ -43,25 +47,28 @@ for user in usersList:
                 name = j['name']
             else:
                 #Combining each song with its artist to create a more complete 'name' tag, it only works for albums and tracks
-                name = j['name'] + ' - ' + j['artist']['name']
+                name = f'{j["name"]} - {j["artist"]["name"]}'
             #It creates a new dictionary to make the count easier
             key = {'name': name, 'playcount': int(j['playcount'])}
             dictList.append(key)
 
 #Grouping each track and adding them, more like combining
 get_name = itemgetter('name')
-dictList = [{'name': name, 'playcount': sum(d['playcount'] for d in dicts)} for name, dicts in groupby(sorted(dictList, key=get_name), key=get_name)]
+dictList = [{
+    'name': name,
+    'playcount': sum(d['playcount'] for d in dicts)
+} for name, dicts in groupby(sorted(dictList, key=get_name), key=get_name)]
+
 
 #Sorting each song by its playcount
 def get_playcount(track):
     return track.get('playcount')
+
+
 dictList.sort(key=get_playcount, reverse=True)
 
 #Printing the top 10
 n = 0
-for s in dictList:
-    if n < 10:
-        n = n + 1
-        print('#' + str(n), s['name'], '[' + str(s['playcount']) + ']')
-    else:
-        break
+for s in dictList[0:10]:
+    n += 1
+    print(f'#{n}. {s["name"]} - [{s["playcount"]}]')
